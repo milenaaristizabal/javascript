337f29f22cfa0275811c83e9f28f453ceef57aea
milenaaristizabal <maristizabal978@misena.edu.co>
2022-08-20 17:17:03 -0500

    primeros ejercicios de javascript

---
+++ js/index.js
@@ -0,0 +1,75 @@
+/*
+
+//1. GRADOS
+
+//entradas
+let gradosFahrenheit = prompt("Ingrese los grados Fahrenheit")
+gradosFahrenheit=parseInt(gradosFahrenheit)
+
+//procesos
+
+let gradosC = (gradosFahrenheit - 32)/(1.8)
+
+//Resultado
+alert("los grados celcius son: " + gradosC + " ºC")
+
+*/
+
+
+//2. ALCANCIA
+
+/*window.confirm("Cuando desee dejar de guardar dinero digite el número 1")
+let cantidad 
+let suma = 0;
+
+while(cantidad != '1'){
+    cantidad = prompt('Ingrese la cantidad que quiere guardar')
+    cantidad=parseInt(cantidad)
+    suma = (suma+cantidad)
+
+}
+alert("Usted ha finalizado este proceso, La cantidad de dinero que ha guardado es: "+suma+ "$")
+
+*/
+
+
+
+/*
+
+//3. EDAD
+
+let edad
+edad = prompt('Ingrese su edad')
+
+if (edad >= 18){
+    alert("Felicidades usted es mayor de edad: "+edad);
+}
+else if(edad > 1){
+    alert("Usted es menor de edad, vayase a dormir: "+edad);
+}
+
+else if(edad == 0){
+    alert("Esta edad es inválida, ingrese una edad real: "+edad);
+}
+*/
+
+
+/*
+
+//4. DATOS PERSONALES
+
+//entradas
+
+let nombre, apellido, edad, telefono
+
+nombre = prompt("Ingrese su nombre")
+apellido = prompt("Ingrese su apellido")
+edad = prompt("Ingrese su edad")
+telefono = prompt("Ingrese su telefono")
+
+
+//Resultado
+alert("su nombre es: " + nombre + " ,su apellido es " + apellido + " ,su edad es " + edad + " ,su telefono es " + telefono)
+
+*/
+
